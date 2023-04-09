300e4f79fc750643aa5c1d0cc42eee504cf7c3c6
esmatsahak <esmatsahak@gmail.com>
2023-04-09 18:39:49 -0400

    Update disparity and depth code

---
+++ depth.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Compute disparity of images
+def computeDisparity(image_left, image_right, num_disparities, block_size, min_disparity):
+    stereo = cv.StereoSGBM_create(numDisparities=num_disparities, blockSize=block_size, minDisparity=min_disparity)
+    disparity = stereo.compute(image_left, image_right).astype(np.float32)/16
+    return disparity
+
+# Compute depth of images
+def computeDepth(disparity_map, T, f):
+    depth = np.divide(f * T, disparity_map, where=disparity_map!=0)
+    return depth
+
+def getCameraParams(calib_file):
+    with open(calib_file) as f:
+        line = f.readlines()[1].split()
+        f = float(line[1])
+        T = -float(line[4])/f 
+    return f, T
+
+if __name__ == "__main__":
+    # Image ID
+    image_id = 'umm_000000'
+
+    # Camera parameters (from calibration file)
+    calib_file = f'data/train/calib/{image_id}.txt'
+    f, T = getCameraParams(calib_file)
+
+    # Read images
+    image_left = cv.imread(f'data/train/image_left/{image_id}.jpg', cv.IMREAD_GRAYSCALE)
+    image_right = cv.imread(f'data/train/image_right/{image_id}.jpg', cv.IMREAD_GRAYSCALE)
+
+    # Compute disparity and visualize
+    disparity = computeDisparity(image_left, image_right, 64, 9, 1)
+    disparity_map = cv.normalize(disparity, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
+    cv.imwrite(f'outputs/disparity_map_{image_id}.jpg', disparity_map)
+
+    # Compute depth and visualize
+    depth = computeDepth(disparity, T, f)
+    depth_map = cv.normalize(depth, None, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+    cv.imwrite(f'outputs/depth_map_{image_id}.jpg', depth)
